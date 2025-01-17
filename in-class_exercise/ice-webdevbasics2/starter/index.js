a94d89a8f2b510068b435956ebb0cee30dc48392
Shanshan WU <ssincn@outlook.com>
2025-01-17 00:06:53 -0500

    finish ice2

---
+++ in-class_exercise/ice-webdevbasics2/starter/index.js
@@ -0,0 +1,33 @@
+// This is where your JS goes!
+
+// You can fetch data from https://cs571api.cs.wisc.edu/rest/f24/ice/chili
+// When you are complete, you should also be able to fetch data from...
+//  https://cs571api.cs.wisc.edu/rest/f24/ice/pasta
+//  https://cs571api.cs.wisc.edu/rest/f24/ice/pizza
+
+let reviewNum = 0;
+
+const BASE_AMNS = [1, 15, 14.5, 2, 1, 1, 1]
+const REVIEWS = [
+    "A burst of warmth and flavor in every spoonful; simple yet irresistible!",
+    "The perfect blend of spice and comfort, an easy go-to chili recipe.",
+    "Loved the hearty texture and rich taste - a new family favorite!",
+    "Quick, flavorful, and satisfying - this chili hits all the right notes!"
+]
+
+
+// TODO Implement the update yield!
+function updateYield() {
+    let servingNum = document.getElementById("serving-selector").value;
+    let rows = document.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
+
+    for (let i=0; i<rows.length; i++){
+        rows[i].getElementsByTagName("td")[0].innerText = BASE_AMNS[i] * parseInt(servingNum);
+    }
+}
+
+// TODO Fix the reviews!
+function displayReview() {
+    alert(REVIEWS[reviewNum]);
+    reviewNum = (reviewNum + 1) % REVIEWS.length;
+}
